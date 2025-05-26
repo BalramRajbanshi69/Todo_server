cb6f113da6474df12fbc4aa6c40d06adf2a31efb
Balram Rajbanshi <balramrajbanshi4769@gmail.com>
2025-05-27 01:13:23 +0545

    server

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors({
   origin: [
     'https://todo-task-app-indol.vercel.app',
     'http://localhost:5173',
-    'http://localhost:8000'
+    'http://127.0.0.1:5173'
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
